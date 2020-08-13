dbec3f2451b6c1cc04d3dd368993a6b8806d324f
林悠暉 <mrchildreny@gmail.com>
2020-08-13 10:22:06 +0900

    port設定変更

--- backend/index.ts
+++ backend/index.ts
@@ -29,4 +29,4 @@ const server = new ApolloServer({
 const app = express();
 server.applyMiddleware({ app });
 
-app.listen({ port: 4000 }, () => console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`));
+app.listen(process.env.PORT || 4000, () => console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`));
