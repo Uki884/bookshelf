022b9c6c25d00c522c50f630bcd06872c0209a50
林悠暉 <mrchildreny@gmail.com>
2020-08-13 16:05:18 +0900

    修正

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -17,6 +17,7 @@ import {
   reactive,
   computed,
 } from "@vue/composition-api"
+import { useUserStore } from '@/store/userStore'
 
 export default defineComponent({
   components: {
@@ -24,6 +25,8 @@ export default defineComponent({
     Index
   },
   setup(props: any, context: SetupContext) {
+    const { useGetCurrentUser, user } = useUserStore()
+    useGetCurrentUser()
     return {
     }
   }
