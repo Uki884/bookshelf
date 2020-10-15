3c9878f2da932243ad17a5e6a57beaf51cd3ffc1
林悠暉 <mrchildreny@gmail.com>
2020-10-15 22:50:37 +0900

    微修正

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -40,13 +40,14 @@ body {
 html,body { height:100%; overflow: hidden; }
 #app {
   height: 100%;
+  width: 100%;
   font-family: 'Noto Serif JP', serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   background: #D7CCC8;
   color: #2c3e50;
-  width: 100%;
+  overflow: scroll;
   -webkit-overflow-scrolling : touch;
 }
 .main-content {
