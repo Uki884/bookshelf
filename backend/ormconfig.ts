68270fea464b37b67ac06dd72d516f4a459a59f7
林悠暉 <mrchildreny@gmail.com>
2021-01-01 22:56:24 +0900

    add docker-compose for mysql

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -4,7 +4,7 @@ module.exports = [{
   name: 'default',
   type: 'mysql',
   host: process.env.DATABASE_URL,
-  port: 3306,
+  port: process.env.DB_PORT || 3306,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
